24d653df86080679a9e34b84408dee96bcaad499
Marcelo Rangel <marcelohar@hotmail.com>
2024-01-18 21:43:47 -0400

    Initial commit

---
+++ src/script.java
@@ -0,0 +1,8 @@
+public class script {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 10;
+        System.out.println(a+b);
+    }
+}
